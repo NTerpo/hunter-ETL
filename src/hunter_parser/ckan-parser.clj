d12c9e5cb5b0ae90b735c282932c49101f480c2e
Terpo <nicolas.terpolilli@gmail.com>
2014-11-03 19:00:58 +0100

    improve filtering parser

--- src/hunter_parser/ckan-parser.clj
+++ src/hunter_parser/ckan-parser.clj
@@ -11,21 +11,24 @@
       :body
       (parse-string true)))
 
+(defn- get-tags
+  [vect]
+  (vec (map #(% :name) (map #(select-keys % [:name]) vect))))
+
 (defn get-10-most-pop-datagov-ds
   []
   (let [response (((get-result "https://catalog.data.gov/api/3/action/package_search?q=&rows=10") :result) :results)
-        filtered (->> (map #(select-keys % [:title
-                                           :notes
-                                           :organization
-                                           :resources
-                                           ;; :extras
-                                           ;; :revision_timestamp
-                                           ]) response)
-                     (map #(assoc % :publisher (get-in % [:organization :title])))
-                     (map #(assoc % :uri (get-in % [:resources 0 :url]))))]
+        filtered (->> (map #(select-keys % [:title :notes :organization :resources :tags :extras :revision_timestamp]) response)
+                      (map #(assoc % :publisher (get-in % [:organization :title])))
+                      (map #(assoc % :uri (get-in % [:resources 0 :url])))
+                      (map #(assoc % :created (get-in % [:resources 0 :created])))
+                      (map #(assoc % :tags (get-tags (% :tags))))
+                      (map #(assoc % :spatial "USA"))
+                      (map #(assoc % :temporal (get-in % [:extras]))))]
     (->> filtered
          (map #(dissoc % :organization))
-         (map #(dissoc % :resources)))))
+         (map #(dissoc % :resources))
+         (map #(dissoc % :extras)))))
 
 
 

61556826cd9c2975cc89f18d3f7a15b624c88e61
Terpo <nicolas.terpolilli@gmail.com>
2014-11-03 16:24:23 +0100

    get 10 most popular ds on data.gov and filter just the title

--- src/hunter_parser/ckan-parser.clj
+++ src/hunter_parser/ckan-parser.clj
@@ -4,19 +4,38 @@
 
 (def base-url "https://catalog.data.gov/api/3/")
 
-(defn get-package-list
-  []
-  (-> (str base-url "action/package_list?limit=1")
+(defn get-result
+  [url]
+  (-> url
       (client/get)
       :body
-      (parse-string)))
+      (parse-string true)))
 
-(defn get-package-with-query
-  [query]
-  (((-> (str base-url "action/package_search?q=")
-         (str query)
-         (client/get)
-         :body
-         (parse-string))
-     "result")
-   "results"))
+(defn get-10-most-pop-datagov-ds
+  []
+  (let [response (((get-result "https://catalog.data.gov/api/3/action/package_search?q=&rows=10") :result) :results)]
+    ;; response
+    (map #(select-keys % [:title]) response)))
+
+
+
+
+
+
+(comment
+  (defn get-package-list
+    []
+    (-> (str base-url "action/package_list?limit=1")
+        (client/get)
+        :body
+        (parse-string)))
+
+  (defn get-package-with-query
+    [query]
+    (((-> (str base-url "action/package_search?q=")
+          (str query)
+          (client/get)
+          :body
+          (parse-string))
+      "result")
+     "results")))

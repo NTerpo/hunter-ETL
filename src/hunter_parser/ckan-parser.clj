f93814b06b16847019221a0b5a68929a941b86d9
Terpo <nicolas.terpolilli@gmail.com>
2014-11-03 22:42:53 +0100

    DONE parser data.gov 10 most popular ds

--- src/hunter_parser/ckan-parser.clj
+++ src/hunter_parser/ckan-parser.clj
@@ -17,45 +17,22 @@
 
 (defn- get-temporal
   [vect]
-  (filter #(not (nil? %))
-          (map #(get % :temporal)
-               (map #(hash-map (keyword (% :key)) (% :value))
-                    (map #(select-keys % [:key :value]) vect)))))
+  (first
+   (filter #(not (nil? %))
+           (map #(get % :temporal)
+                (map #(hash-map (keyword (% :key)) (% :value))
+                     (map #(select-keys % [:key :value]) vect))))))
 
 (defn get-10-most-pop-datagov-ds
   []
-  (let [response (((get-result "https://catalog.data.gov/api/3/action/package_search?q=&rows=10") :result) :results)
-        filtered (->> (map #(select-keys % [:title :notes :organization :resources :tags :extras :revision_timestamp]) response)
-                      (map #(assoc % :publisher (get-in % [:organization :title])))
-                      (map #(assoc % :uri (get-in % [:resources 0 :url])))
-                      (map #(assoc % :created (get-in % [:resources 0 :created])))
-                      (map #(assoc % :tags (get-tags (% :tags))))
-                      (map #(assoc % :spatial "USA"))
-                      (map #(assoc % :temporal (get-temporal (% :extras)))))]
-    (->> filtered
-         (map #(dissoc % :organization))
-         (map #(dissoc % :resources))
-         (map #(dissoc % :extras)))))
-
-
-
-
-
-
-(comment
-  (defn get-package-list
-    []
-    (-> (str base-url "action/package_list?limit=1")
-        (client/get)
-        :body
-        (parse-string)))
-
-  (defn get-package-with-query
-    [query]
-    (((-> (str base-url "action/package_search?q=")
-          (str query)
-          (client/get)
-          :body
-          (parse-string))
-      "result")
-     "results")))
+  (let [response (((get-result "https://catalog.data.gov/api/3/action/package_search?q=&rows=10") :result) :results)]
+    (->> (map #(select-keys % [:title :notes :organization :resources :tags :extras :revision_timestamp]) response)
+         (map #(assoc % :publisher (get-in % [:organization :title])
+                      :uri (get-in % [:resources 0 :url])
+                      :created (get-in % [:resources 0 :created])
+                      :tags (get-tags (% :tags))
+                      :spatial "USA"
+                      :temporal (get-temporal (% :extras))
+                      :updated (% :revision_timestamp)
+                      :description (% :notes)))
+         (map #(dissoc % :organization :resources :extras :revision_timestamp :notes)))))

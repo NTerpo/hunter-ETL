8a9ed29023a445649f2e7b4b3ce1b885424c4186
Terpo <nicolas.terpolilli@gmail.com>
2015-02-12 00:35:36 +0100

    add parser for data.gov.uk

---
+++ src/hunter_parser/datagovuk.clj
@@ -0,0 +1,51 @@
+(ns hunter-parser.datagovuk
+  (:require [clj-http.client :as client]
+            [cheshire.core :refer :all]
+            [clojure.string :as st]
+            [hunter-parser.util :refer :all]))
+
+(def base-url "http://data.gov.uk/api/3/")
+
+(defn get-tags
+  [vect]
+  (vec (map #(% :name) (map #(select-keys % [:name]) vect))))
+
+(defn clean-resources
+  [coll title]
+  (vec (->> (map #(select-keys % [:format :url]) coll)
+            (map #(assoc % :title title)))))
+
+(defn get-datagov-uk-ds
+  "gets a number of the most popular datasets' metadata from the ckan API of data.gov.uk and transforms them to match the Hunter API scheme"
+  [number offset]
+  (let [response (((get-result (str base-url "action/package_search?q="
+                                    "&rows=" number
+                                    "&start=" offset))
+                   :result)
+                  :results)]
+    (->> (map #(select-keys % [:title :notes :organization :resources :tags :tracking_summary :temporal_coverage-to
+                               :metadata_created :metadata_modified :temporal_coverage-from :geographic_coverage :url]) response)
+         (map #(assoc %
+                 :description (if-not (nil? (% :notes))
+                                (% :notes)
+                                (% :title))
+                 :uri (if-not (nil? (% :url))
+                        (% :url)
+                        "URI Not Available")
+                 :publisher (get-in % [:organization :title])
+                 :created (% :metadata_created) ; TODO: check nil
+                 :updated (% :metadata_modified) ; TODO: check nil
+                 :spatial (geo-tagify "uk") ; TODO: expend UK
+                 :temporal (extend-temporal (str (% :temporal_coverage-from) "/"
+                                                 (% :temporal_coverage-to)))
+                 :tags (vec (concat (tagify-title (% :title)) ; TODO: check nil
+                                    (extend-tags (get-tags (% :tags)))))
+                 :resources (clean-resources (% :resources) (% :title))
+                 :huntscore (calculate-huntscore 0 ; TODO: find a way
+                                        ; to give a score
+                                                 (get-in % [:tracking_summary :total])
+                                                 (get-in % [:tracking_summary :recent])
+                                                 0)))
+         (map #(dissoc % :notes :temporal_coverage-to :temporal_coverage-from :tracking_summary
+                       :metadata_created :metadata_modified :geographic_coverage :url :organization)))))
+
